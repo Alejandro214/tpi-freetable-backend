0dd808328518fb1c70cd6b8be64eabfe74e90c09
Luis Mamani <luis.mamani@eldars.com.ar>
2022-05-09 19:28:01 -0300

    agrego el endpoint de guardar pedido

--- src/main/resources/db/migration/V2.0__create_order_table.sql
+++ src/main/resources/db/migration/V2.0__create_order_table.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS `table_order`;
 CREATE TABLE `table_order` (
   `idOrder` int NOT NULL AUTO_INCREMENT,
   `totalPrice` double NOT NULL,
+  `idMesa` int,
   PRIMARY KEY (`idOrder`)
 )
 
