c0f7c2799235bd744022814fb79d07edeb0ceac4
Luis Mamani <luis.mamani@eldars.com.ar>
2022-05-20 18:37:59 -0300

    prueba 8

--- src/test/java/com/restaurant/restaurantApi/ProductServiceImplTest.java
+++ src/test/java/com/restaurant/restaurantApi/ProductServiceImplTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
@@ -22,7 +23,7 @@ import static java.util.Arrays.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-
+@SpringBootTest
 public class ProductServiceImplTest {
 
     @Mock
