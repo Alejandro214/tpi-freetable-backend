810ec21cb15b3e56ba85ab1d165693195a308de4
Luis Mamani <luis.mamani@eldars.com.ar>
2022-05-20 19:05:42 -0300

    prueba de integracion

--- src/test/java/com/restaurant/restaurantApi/ProductServiceImplTest.java
+++ src/test/java/com/restaurant/restaurantApi/ProductServiceImplTest.java
@@ -23,7 +23,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-@TestComponent
 public class ProductServiceImplTest {
 
     @Mock
