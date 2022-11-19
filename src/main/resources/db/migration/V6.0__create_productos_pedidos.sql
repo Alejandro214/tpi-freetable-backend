076e8df260d4196e19f36b0a4221c26c8b564d17
Luis Mamani <luis.mamani@eldars.com.ar>
2022-11-18 22:02:15 -0300

    correccion de insert a la tabla pedidos_productos

--- src/main/resources/db/migration/V6.0__create_productos_pedidos.sql
+++ src/main/resources/db/migration/V6.0__create_productos_pedidos.sql
@@ -6,8 +6,12 @@ CREATE TABLE productos_pedidos (
   cantProduct INT NOT NULL DEFAULT 1,
   PRIMARY KEY (id_product,id_order)
 );
+
+ALTER TABLE productos_pedidos ADD CONSTRAINT pp_fk FOREIGN KEY (id_product) REFERENCES product(idProduct) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE productos_pedidos ADD CONSTRAINT pp_fk1 FOREIGN KEY (id_order) REFERENCES table_order(idOrder) ON DELETE CASCADE ON UPDATE CASCADE;
+
 INSERT INTO productos_pedidos (id_product,id_order,cantProduct) VALUES (18,1,1);
 INSERT INTO productos_pedidos (id_product,id_order,cantProduct) VALUES (21,1,1);
 
 INSERT INTO productos_pedidos (id_product,id_order,cantProduct) VALUES (18,2,1);
-INSERT INTO productos_pedidos (id_product,id_order,cantProduct) VALUES (15,2,1);
+INSERT INTO productos_pedidos (id_product,id_order,cantProduct) VALUES (21,2,1);
